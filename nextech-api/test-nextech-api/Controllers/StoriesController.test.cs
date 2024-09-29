1e15b2bcd3b7aadfaa97de02f638bd4ab3a0050d
Michael Wester <mpwester@gmail.com>
2024-09-29 07:53:12 -0500

    Add mock data and Test Project

---
+++ nextech-api/test-nextech-api/Controllers/StoriesController.test.cs
@@ -0,0 +1,17 @@
+﻿namespace test_nextech_api;
+
+public class UnitTest1
+{
+    class DummyService
+    {
+        public List<String> GuestTypes { get; set; }
+
+        public List<String> GetGuestType() => GuestTypes;
+    }
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
